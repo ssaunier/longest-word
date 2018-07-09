7a767cb4925376338720828f74201924ea872dbf
Sébastien Saunier <seb@saunier.me>
2018-07-09 21:13:45 +0200

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 
 import string
 import random
+import requests
 
 class Game:
     GRID_SIZE = 9
@@ -19,4 +20,9 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    def __check_dictionary(self, word):
+        r = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        response = r.json()
+        return response['found']
--- tests/test_game.py
+++ tests/test_game.py
@@ -23,7 +23,7 @@ class TestGame(unittest.TestCase):
         self.assertIs(new_game.is_valid('SANDWICH'), False)
         self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # Make sure the grid remained untouched
 
-    def test_is_invalid(self):
+    def test_unknown_word_is_invalid(self):
       new_game = Game()
       new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
       self.assertIs(new_game.is_valid('FEUN'), False)
